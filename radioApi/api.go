00d1bbe4cf3054a4ce5330f97d5e5167333361b2
David <david@likeafox.org>
2022-05-03 22:36:12 +0100

    radio player test

--- radioApi/api.go
+++ radioApi/api.go
@@ -1,54 +1,22 @@
 package main
 
 import (
-	"bytes"
-	"encoding/binary"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"time"
-
-	"github.com/gordonklaus/portaudio"
+	"io"
+	"os/exec"
 )
 
-// see:
-// https://github.com/valentijnnieman/audio_streamer/blob/master/audio_streamer_client/main.go
+// example command:
+// wget -O - http://stream.live.vc.bbcmedia.co.uk/bbc_radio_one | mpv -
 
-// streams:
+// here are some streams:
 // https://garfnet.org.uk/cms/tables/radio-frequencies/internet-radio-player/
 
-const sampleRate = 44100
-const seconds = 2
-
 func main() {
-	portaudio.Initialize()
-	defer portaudio.Terminate()
-	buffer := make([]float32, sampleRate*seconds)
-
-	stream, err := portaudio.OpenDefaultStream(0, 1, sampleRate, len(buffer), func(out []float32) {
-		resp, err := http.Get("http://stream.live.vc.bbcmedia.co.uk/bbc_radio_one")
-		chk(err)
-		body, _ := ioutil.ReadAll(resp.Body)
-		responseReader := bytes.NewReader(body)
-		binary.Read(responseReader, binary.BigEndian, &buffer)
-		for i := range out {
-			out[i] = buffer[i]
-		}
-	})
-	chk(err)
-	chk(stream.Start())
-	time.Sleep(time.Second * 40)
-	chk(stream.Stop())
-	defer stream.Close()
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-}
-
-func chk(err error) {
-	if err != nil {
-		panic(err)
-	}
+	wget := exec.Command(
+		"wget", "-O", "-", "http://stream.live.vc.bbcmedia.co.uk/bbc_radio_one",
+	)
+	mpv := exec.Command("mpv", "-")
+	mpv.Stdin, wget.Stdout = io.Pipe()
+	wget.Start()
+	mpv.Run()
 }
