dada905b77890720c5a15f2e853072d09904e54b
David <david@likeafox.org>
2022-05-19 20:52:23 +0100

    switch to github.com/tosone/minimp3

--- radioApi/api/player.go
+++ radioApi/api/player.go
@@ -2,46 +2,155 @@ package main
 
 import (
 	"io"
-	"os"
-	"os/exec"
-	"time"
+	"log"
+	"net/http"
+	"sync"
 
+	"github.com/gin-gonic/gin"
+	"github.com/hajimehoshi/oto"
 	"github.com/itchyny/volume-go"
+	"github.com/tosone/minimp3"
 )
 
-// example command:
-// wget -O - http://stream.live.vc.bbcmedia.co.uk/bbc_radio_one | mpv -
+var dec *minimp3.Decoder // global mp3 decoder
+var stop chan bool       // for sending stop signals
 
-// here are some streams:
-// https://garfnet.org.uk/cms/tables/radio-frequencies/internet-radio-player/
+type PlayRequest struct {
+	Url string `json:"url"`
+}
 
-func play(url string, ch chan os.Process) {
-	wget := exec.Command("wget", "-O", "-", url)
-	mpv := exec.Command("mpv", "-")
-	mpv.Stdin, wget.Stdout = io.Pipe()
-	wget.Start()
-	mpv.Start()
-	ch <- *wget.Process
-	ch <- *mpv.Process
+type VolumeRequest struct {
+	Command string `json:"command"`
+}
+
+func playStream(url string) *minimp3.Decoder {
+	var err error
+	var response *http.Response
+
+	if response, err = http.Get(url); err != nil {
+		log.Fatal(err)
+		return nil
+	}
+
+	var dec *minimp3.Decoder
+	if dec, err = minimp3.NewDecoder(response.Body); err != nil {
+		log.Fatal(err)
+		return nil
+	}
+	<-dec.Started()
+
+	log.Printf("Convert audio sample rate: %d, channels: %d\n", dec.SampleRate, dec.Channels)
+
+	var waitForPlayOver = new(sync.WaitGroup)
+	waitForPlayOver.Add(1)
+
+	go func() {
+		defer func() {
+			dec.Close()
+			dec = nil
+			response.Body.Close()
+		}()
+
+		var context *oto.Context
+		if context, err = oto.NewContext(dec.SampleRate, dec.Channels, 2, 4096); err != nil {
+			log.Fatal(err)
+			return
+		}
+		defer context.Close()
+		var player = context.NewPlayer()
+		defer player.Close()
+		for {
+			var data = make([]byte, 512)
+			_, err = dec.Read(data)
+			if len(stop) > 0 {
+				log.Println("Stop signal waiting")
+				break
+			}
+			if err == io.EOF {
+				log.Println("EOF")
+				break
+			}
+			if err != nil {
+				log.Fatal(err)
+				break
+			}
+			player.Write(data)
+		}
+		log.Println("Streaming stopped")
+		waitForPlayOver.Done()
+
+		if len(stop) > 0 {
+			<-stop
+			log.Println("Stop signal consumed")
+		} else {
+			go playStream(url) // try to restart a faulted stream
+		}
+	}()
+
+	return dec
+}
+
+func postVol(c *gin.Context) {
+	var request VolumeRequest
+
+	if err := c.BindJSON(&request); err != nil {
+		c.String(http.StatusBadRequest, "Bad request, could not get volume command")
+		return
+	}
+
+	vol, err := volume.GetVolume()
+	if err != nil {
+		c.String(http.StatusInternalServerError, "Could not get volume")
+		return
+	}
+
+	if request.Command == "+" {
+		vol++
+	}
+	if request.Command == "-" {
+		vol--
+	}
+	err = volume.SetVolume(vol)
+	if err != nil {
+		c.String(http.StatusInternalServerError, "Could not set volume")
+		return
+	}
+	c.String(http.StatusOK, string(vol))
+}
+
+func postPlay(c *gin.Context) {
+	var request PlayRequest
+
+	if err := c.BindJSON(&request); err != nil {
+		c.String(http.StatusBadRequest, "Bad request, could not get URL")
+		return
+	}
+
+	if dec = playStream(request.Url); dec != nil {
+		c.String(http.StatusOK, "OK")
+		return
+	}
+
+	c.String(http.StatusInternalServerError, "Could not play radio")
+}
+
+func postStop(c *gin.Context) {
+	if dec == nil {
+		c.String(http.StatusBadRequest, "Nothing to stop")
+		return
+	}
+	if len(stop) == 0 {
+		stop <- true // signal a stop
+	}
+	c.String(http.StatusOK, "OK")
 }
 
 func main() {
-	vol, err := volume.GetVolume()
-	if err != nil {
-		panic("cannot get volume info")
-	}
-	err = volume.SetVolume(vol / 2)
-	if err != nil {
-		panic("cannot set volume")
-	}
+	stop = make(chan bool, 1)
 
-	ch := make(chan os.Process)
-	go play("http://stream.live.vc.bbcmedia.co.uk/bbc_radio_one", ch)
-	wget := <-ch
-	mpv := <-ch
-
-	time.Sleep(time.Second * 30)
-
-	mpv.Kill()
-	wget.Kill()
+	engine := gin.Default()
+	engine.POST("/api/play", postPlay)
+	engine.POST("/api/stop", postStop)
+	engine.POST("/api/volume", postVol)
+	engine.Run(":8080")
 }
--- radioApi/go.mod
+++ radioApi/go.mod
@@ -2,9 +2,31 @@ module main
 
 go 1.18
 
-require github.com/itchyny/volume-go v0.2.1
+require (
+	github.com/gin-gonic/gin v1.7.7
+	github.com/hajimehoshi/oto v1.0.1
+	github.com/itchyny/volume-go v0.2.1
+	github.com/tosone/minimp3 v1.0.1
+)
 
 require (
+	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-ole/go-ole v1.2.4 // indirect
+	github.com/go-playground/locales v0.13.0 // indirect
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/golang/protobuf v1.3.3 // indirect
+	github.com/json-iterator/go v1.1.9 // indirect
+	github.com/leodido/go-urn v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
 	github.com/moutend/go-wca v0.2.0 // indirect
+	github.com/ugorji/go/codec v1.1.7 // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	golang.org/x/exp v0.0.0-20190306152737-a1d7652674e8 // indirect
+	golang.org/x/image v0.0.0-20190227222117-0694c2d4d067 // indirect
+	golang.org/x/mobile v0.0.0-20190415191353-3e0bab5405d6 // indirect
+	golang.org/x/sys v0.0.0-20220503163025-988cb79eb6c6 // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
